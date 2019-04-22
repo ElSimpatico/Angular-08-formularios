56a80fa5122b710eba2277ac7667258399df5384
Aaron Velasco <tiquismiquisss@gmail.com>
2019-04-22 17:07:17 +0200

    obteniendo la info del formulario

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -6,14 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class TemplateComponent implements OnInit {
-
+  usuario: Object = {
+    nombre: 'Aarón',
+    apellido:  'Velasco',
+    email: 'tiquismqiquisss@gmail.com'
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
-  guardar() {
+  guardar(forma: NgForm) {
     console.log('Formulario posteado');
+    console.log('ngForm', forma);
+    console.log('valor', forma.value);
+    console.log('usuario', this.usuario);
   }
 
 }
